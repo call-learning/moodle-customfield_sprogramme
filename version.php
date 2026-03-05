dd248cee58a290f97c1fa3250200889ee33126a2
Laurent David <lmedavid@gmail.com>
2026-03-05 18:20:14 +0100

    Fix #820 Finalize visa icon and dialog UI updates

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'customfield_sprogramme';
 $plugin->release = '2.3.0';
-$plugin->version = 2026020802;
+$plugin->version = 2026030503;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_BETA;
